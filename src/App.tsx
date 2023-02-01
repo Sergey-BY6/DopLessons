1c5d159f59000a6b82a8b03c7f7b18edda05f798
Sergey Babich <sergei.babich2022@yandex.by>
2023-02-01 19:34:55 +0300

    Destructive assignment, lesson 05.

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,146 @@
 import React, {useState} from 'react';
 import './App.css';
-import {SuperButton} from './SuperButton/SuperButton';
-import {SuperTodoList} from './SuperButton/SuperTodoList';
+import {TaskType, Todolist} from './Todolist';
+import {v1} from 'uuid';
+// import {v1} from 'uuid';
+
+export type FilterValuesType = "all" | "active" | "completed";
+
+
+type TodolistsType = { id: string, title: string}
+
+type TasksStateType = {
+    [key: string]: {
+        data: TaskType[]
+            filter: FilterValuesType
+    }
+}
+
 
 
 function App() {
+    // let todolistID1 = v1();
+    // let todolistID2 = v1();
+    //
+    // let [todolists, setTodolists] = useState<Array<TodolistsType>>([
+    //     {id: todolistID1, title: 'What to learn', filter: 'all'}, //0
+    //      {id: todolistID2, title: 'What to buy', filter: 'all'},  //1
+    // ])
+    //
+    // let [tasks, setTasks] = useState({
+    //     [todolistID1]: [
+    //         {id: v1(), title: "HTML&CSS", isDone: true},
+    //         {id: v1(), title: "JS", isDone: true},
+    //         {id: v1(), title: "ReactJS", isDone: false},
+    //         {id: v1(), title: "Rest API", isDone: false},
+    //         {id: v1(), title: "GraphQL", isDone: false},
+    //     ],
+    //     [todolistID2]: [
+    //         {id: v1(), title: "HTML&CSS2", isDone: true},
+    //         {id: v1(), title: "JS2", isDone: true},
+    //         {id: v1(), title: "ReactJS2", isDone: false},
+    //         {id: v1(), title: "Rest API2", isDone: false},
+    //         {id: v1(), title: "GraphQL2", isDone: false},
+    //     ]
+    // });
 
-    const tasks = [
-        { id: 1, title: "HTML&CSS", isDone: true },
-        { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false }
-    ]
+    let todolistId1 = v1();
+    let todolistId2 = v1();
+
+    let [todolists, setTodolists] = useState<Array<TodolistsType>>([
+        {id: todolistId1, title: "What to learn"},
+        {id: todolistId2, title: "What to buy"}
+    ])
+
+    let [tasks, setTasks] = useState<TasksStateType>({
+        [todolistId1]: {
+            data: [
+                {id: v1(), title: "HTML&CSS1111", isDone: true},
+                {id: v1(), title: "JS1111", isDone: true}
+            ],
+            filter: "all"
+        },
+        [todolistId2]: {
+            data: [
+                {id: v1(), title: "HTML&CSS22222", isDone: true},
+                {id: v1(), title: "JS2222", isDone: true}
+            ],
+            filter: "all"
+        }
+    });
+
+
+    const removeTodolist = (todolistId: string) => {
+
+        setTodolists(todolists.filter(el => el.id !==todolistId))
+        delete tasks[todolistId]
+        // setTodolists(todolists.filter(el => el.id !== todolistId))
+        // delete tasks[todolistId]
+        // console.log(tasks)
+    }
+
+
+    function removeTask(todolistId: string, taskId: string) {
+        //setTasks({...tasks, [todolistId]: tasks[todolistId].filter(el => el.id !== taskId)})
+        setTasks({...tasks, [todolistId]: {...tasks[todolistId], data: tasks[todolistId].data.filter(el => el.id !== taskId)}})
+    }
+
+
+    function addTask(todolistId: string, title: string) {
+        let newTask = {id: v1(), title: title, isDone: false};
+
+        setTasks({...tasks, [todolistId]: {...tasks[todolistId], data: [newTask, ...tasks[todolistId].data, ]} })
+
+        // let newTask = {id: v1(), title: title, isDone: false};
+        // setTasks({...tasks, [todolistId]: [...tasks[todolistId], newTask]})
+        // let newTasks = [task, ...tasks];
+        // setTasks(newTasks);
+    }
+
+    function changeStatus(todolistId: string, taskId: string, newIsDone: boolean) {
+
+
+        setTasks({...tasks, [todolistId]: {...tasks[todolistId], data: tasks[todolistId].data.map(el => el.id === taskId ? {...el, isDone: newIsDone} : el) }})
+        // setTasks({
+        //     ...tasks,
+        //     [todolistId]: tasks[todolistId].map(el => el.id === taskId ? {...el, isDone: newIsDone} : el)
+        // })
+    }
+
+    function changeFilter(todolistId: string, value: FilterValuesType) {
+        setTasks({...tasks, [todolistId]: {...tasks[todolistId], filter: value}})
+        // setTodolists(todolists.map(el => el.id === todolistId ? {...el, filter: value} : el))
+    }
 
     return (
-        <div>
-
-
-            {/*    <SuperButton callback={() => {}} color={'red'}>RED SUPER BUTTON</SuperButton>*/}
-            {/*    <SuperButton callback={() => {}} >DEFAULT SUPER BUTTON</SuperButton>*/}
-            {/*    <SuperButton callback={() => {}} color={'secondary'} disabled>RED SUPER BUTTON</SuperButton>*/}
-
-
-            <SuperTodoList tasks={tasks}>
-                <SuperButton callback={() => {}} color={'red'}>RED SUPER BUTTON</SuperButton>
-                <SuperButton callback={() => {}} >DEFAULT SUPER BUTTON</SuperButton>
-                <SuperButton callback={() => {}} color={'secondary'} disabled>RED SUPER BUTTON</SuperButton>
-                <img src="https://static.kupindoslike.com/Adidas-ZX-750-muske-patike-besplatna-dostava_slika_O_2883884.jpg" alt="image" width={"500px"}/>
-
-            </SuperTodoList>
-
-
-            <SuperTodoList tasks={tasks}>
-                <SuperButton callback={() => {}} color={'red'}>RED SUPER BUTTON</SuperButton>
-                <SuperButton callback={() => {}} >DEFAULT SUPER BUTTON</SuperButton>
-                <SuperButton callback={() => {}} color={'secondary'} disabled>RED SUPER BUTTON</SuperButton>
-                <img src="https://outmaxshop.ru/components/com_jshopping/files/img_products/14220/adidas-marathon-tr-19-14220-5.jpg" alt="image" width={"450px"}/>
-
-
-            </SuperTodoList>
-
-            <SuperTodoList tasks={tasks}>
-                <SuperButton callback={() => {}} color={'red'}>RED SUPER BUTTON</SuperButton>
-                <SuperButton callback={() => {}} >DEFAULT SUPER BUTTON</SuperButton>
-                <SuperButton callback={() => {}} color={'secondary'} disabled>RED SUPER BUTTON</SuperButton>
-                <img src="https://images.ua.prom.st/451454349_w640_h640_krossovki-nike-air.jpg" alt="image" width={"500px"}/>
-                
-            </SuperTodoList>
+        <div className="App">
+            {todolists.map((el) => {
+                let tasksForTodolist = tasks[el.id].data;
+                if (tasks[el.id].filter  === "active") {
+                    tasksForTodolist = tasks[el.id].data.filter(t => t.isDone === false);
+                }
+                if (tasks[el.id].filter === "completed") {
+                    tasksForTodolist = tasks[el.id].data.filter(t => t.isDone === true);
+                }
+                return (
+                    <Todolist
+                        key={el.id}
+                        todolistId={el.id}
+                        title={el.title}
+                        tasks={tasksForTodolist}
+                        removeTask={removeTask}
+                        changeFilter={changeFilter}
+                        addTask={addTask}
+                        changeTaskStatus={changeStatus}
+                        filter={tasks[el.id].filter}
+                        removeTodolist={removeTodolist}
+                    />
+                )
+            })}
 
 
         </div>
-
-    )
+    );
 }
-
 
 export default App;
